04df894fbbf684a9e10c91890e3d325d49b61582
FlorentinaDervishaj <florentina.dervishaj@student.uni-pr.edu>
2026-05-16 16:18:49 +0200

    pjesa e mailbox_dir

---
+++ server.py
@@ -0,0 +1,141 @@
+import socket
+import threading
+import json
+import os
+from datetime import datetime
+
+HOST = "127.0.0.1"
+PORT = 5000
+MAILBOX_DIR = "mailboxes"
+
+if not os.path.exists(MAILBOX_DIR):
+    os.makedirs(MAILBOX_DIR)
+
+
+def log(message):
+    print(f"[SERVER {datetime.now().strftime('%H:%M:%S')}] {message}")
+
+
+def handle_client(conn, addr):
+
+    log(f"Client connected: {addr}")
+
+    try:
+
+        data = conn.recv(100000).decode()
+
+        request = json.loads(data)
+
+        action = request.get("action")
+
+        if action == "send":
+
+            sender = request["sender"]
+            receiver = request["receiver"]
+
+            email_data = {
+                "sender": sender,
+                "receiver": receiver,
+                "package": request["package"],
+                "time": str(datetime.now())
+            }
+
+            save_email(receiver, email_data)
+
+            log(f"New encrypted email received from {sender}")
+            log(f"Email forwarded to {receiver}")
+
+            response = {
+                "status": "success",
+                "message": "Email sent successfully."
+            }
+
+        elif action == "receive":
+
+            receiver = request["receiver"]
+
+            emails = get_emails(receiver)
+
+            log(f"{receiver} checked mailbox.")
+
+            response = {
+                "status": "success",
+                "emails": emails
+            }
+
+        else:
+
+            response = {
+                "status": "error",
+                "message": "Invalid action."
+            }
+
+        conn.send(json.dumps(response).encode())
+
+    except Exception as e:
+
+        response = {
+            "status": "error",
+            "message": str(e)
+        }
+
+        conn.send(json.dumps(response).encode())
+
+    finally:
+        conn.close()
+
+
+def start_server():
+
+    server = socket.socket(
+        socket.AF_INET,
+        socket.SOCK_STREAM
+    )
+
+    server.bind((HOST, PORT))
+
+    server.listen()
+
+    log("Email server started...")
+
+
+    
+
+    MAILBOX_DIR = "mailboxes"
+
+if not os.path.exists(MAILBOX_DIR):
+    os.makedirs(MAILBOX_DIR)
+
+
+def save_email(receiver, email_data):
+
+    mailbox_path = f"{MAILBOX_DIR}/{receiver}.json"
+
+    if os.path.exists(mailbox_path):
+
+        with open(mailbox_path, "r") as f:
+            emails = json.load(f)
+
+    else:
+        emails = []
+
+    emails.append(email_data)
+
+    with open(mailbox_path, "w") as f:
+        json.dump(emails, f, indent=4)
+
+
+def get_emails(receiver):
+
+    mailbox_path = f"{MAILBOX_DIR}/{receiver}.json"
+
+    if not os.path.exists(mailbox_path):
+        return []
+
+    with open(mailbox_path, "r") as f:
+        emails = json.load(f)
+
+    with open(mailbox_path, "w") as f:
+        json.dump([], f)
+
+    return emails
